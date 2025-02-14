44fa3a9d1ac702bc661d5173c112a8aa92eb4f1e
Vinicius Siqueira <98608029+viniciussiqueira-dev@users.noreply.github.com>
2025-02-14 18:27:04 -0300

    Initial commit

---
+++ src/test/java/me/dio/avanade_tech/AvanadeTechApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.avanade_tech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvanadeTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
